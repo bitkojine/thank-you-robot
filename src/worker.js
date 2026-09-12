eec5da5cf6c23a2586b7b9765171f2e554fd3194
bitkojine <lanky-those-outlet@duck.com>
2026-09-13 00:51:31 +0300

    Redirect game route to trailing slash

--- src/worker.js
+++ src/worker.js
@@ -3,6 +3,10 @@ export default {
     const incoming = new URL(request.url);
 
     if (incoming.pathname.startsWith("/thank-you-robot")) {
+      if (incoming.pathname === "/thank-you-robot") {
+        incoming.pathname = "/thank-you-robot/";
+        return Response.redirect(incoming, 301);
+      }
       const rewritten = new URL(request.url);
       rewritten.pathname = incoming.pathname.replace(/^\/thank-you-robot/, "") || "/";
       return env.ASSETS.fetch(new Request(rewritten, request));
